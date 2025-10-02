213487de9ace0096ed66ea300301c54e4cd793a7
Alexan50 <1557071@senati.pe>
2025-10-02 10:41:03 -0500

    Archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/bienvenido', function () 
+{
+    return view('bienvenidos');
+});
